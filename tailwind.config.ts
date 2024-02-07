e9185b419903da33b9c3faee1b3928f3455d6ac9
Hrithik <slumdog4321@gmail.com>
2024-02-07 23:58:44 +0530

    font and bg color set

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,11 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    fontFamily: {
+      sans: '"SF Pro Display",-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen,Ubuntu, Cantarell,"Open Sans","Helvetica Neue",sans-serif',
+    },
+    colors: {
+      background: "#000212",
     },
   },
   plugins: [],
