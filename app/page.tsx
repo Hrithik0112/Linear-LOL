b68e23600a71ae75d63df7a16c61299a1ca033af
Hrithik <slumdog4321@gmail.com>
2024-02-09 14:55:03 +0530

    sections added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import { Button, IconWrapper } from "@/components/button";
-import { Clients } from "@/components/clients";
+import { Clients } from "@/components/sections/clients";
 import { Container } from "@/components/container";
-import { Hero, HeroSubtitle, HeroTitle } from "@/components/hero";
-import { HeroImage } from "@/components/hero-image";
-import { ChevronIcon } from "@/components/icons/chevron";
 import { StarsIllustration } from "@/components/icons/stars";
+import { HomepageHero } from "@/components/sections/homepage-hero";
+import { UnlikeAnyTool } from "@/components/sections/unlike-any-tool";
 import classNames from "classnames";
 
 export default function Home() {
@@ -12,36 +10,7 @@ export default function Home() {
     <>
       <div className="overflow-hidden">
         <Container className=" py-[6.4rem]">
-          <Hero>
-            <Button
-              className="translate-y-[-1rem] animate-fade-in opacity-0"
-              href="/"
-              variant="secondary"
-              size="small"
-            >
-              Linear 2022 Release – Built for scale <IconWrapper>→</IconWrapper>
-            </Button>
-            <HeroTitle className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:200ms]">
-              Linear is a better way
-              <br className="hidden md:block" /> to build products
-            </HeroTitle>
-            <HeroSubtitle className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms]">
-              Meet the new standard for modern software development.
-              <br className="hidden md:block" /> Streamline issues, sprints, and product roadmaps.
-            </HeroSubtitle>
-            <Button
-              className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:600ms]"
-              href="/"
-              variant="primary"
-              size="large"
-            >
-              Get Started{" "}
-              <IconWrapper>
-                <ChevronIcon />
-              </IconWrapper>
-            </Button>
-            <HeroImage />
-          </Hero>
+          <HomepageHero />
         </Container>
       </div>
       <Container>
@@ -56,6 +25,9 @@ export default function Home() {
       >
         <StarsIllustration />
       </div>
+      <Container>
+        <UnlikeAnyTool />
+      </Container>
     </>
   );
 }
