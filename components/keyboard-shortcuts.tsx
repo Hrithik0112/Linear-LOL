bdcf5fa5365fc21f62215adffbddd155c31b7886
Hrithik <slumdog4321@gmail.com>
2024-02-09 19:05:22 +0530

    keyboard added and button & link recognize

---
+++ components/keyboard-shortcuts.tsx
@@ -0,0 +1,9 @@
+import { KeyboardIllustration } from "./illustration/keyboard";
+
+export const KeyboardShortcut = () => {
+  return (
+    <div className="mask-keyboard">
+      <KeyboardIllustration />
+    </div>
+  );
+};
