764069724b2e94ec15160c82267ab89335918c42
Hrithik <slumdog4321@gmail.com>
2024-02-09 14:49:41 +0530

    added start illustration

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import { Container } from "@/components/container";
 import { Hero, HeroSubtitle, HeroTitle } from "@/components/hero";
 import { HeroImage } from "@/components/hero-image";
 import { ChevronIcon } from "@/components/icons/chevron";
+import { StarsIllustration } from "@/components/icons/stars";
+import classNames from "classnames";
 
 export default function Home() {
   return (
@@ -45,6 +47,15 @@ export default function Home() {
       <Container>
         <Clients />
       </Container>
+      <div
+        className={classNames(
+          "z-[-1] mask-radial-faded pointer-events-none relative my-[-12.8rem] h-[60rem] overflow-hidden",
+          "[--color:#7877C6] before:absolute before:inset-0 before:bg-radial-faded before:opacity-[0.4]",
+          "after:absolute after:top-1/2 after:-left-1/2 after:h-[142.8%] after:w-[200%] after:rounded-[50%] after:border-t after:border-[rgba(120,_119,_198,_0.4)] after:bg-background"
+        )}
+      >
+        <StarsIllustration />
+      </div>
     </>
   );
 }
