37a4ce13f921c6ae4b0b57793ce3abda891679f0
Hrithik <slumdog4321@gmail.com>
2024-02-09 13:17:43 +0530

    added hero image and animations

--- components/button.tsx
+++ components/button.tsx
@@ -1,17 +1,25 @@
 import Link from "next/link";
 import { cva, VariantProps } from "class-variance-authority";
+import { AnchorHTMLAttributes } from "react";
 
-interface ButtonProps extends VariantProps<typeof buttonClasses> {
+interface ButtonProps
+  extends VariantProps<typeof buttonClasses>,
+    AnchorHTMLAttributes<HTMLAnchorElement> {
   children: React.ReactNode;
   href: string;
 }
 
-const buttonClasses = cva("rounded-full inline-flex items-center", {
+const buttonClasses = cva("relative rounded-full inline-flex items-center", {
   variants: {
     variant: {
-      primary: "bg-primary-gradient hover:text-shadow hover:shadow-primary",
-      secondary: "",
-      tertiary: "",
+      primary: [
+        "bg-primary-gradient hover:text-shadow hover:shadow-primary transition-[shadow,text-shadow]",
+        "[&_.icon-wrapper]:ml-2",
+      ],
+      secondary: [
+        "text-off-white bg-white bg-opacity-10 border border-transparent-white backdrop-filter-[12px] hover:bg-opacity-20 transition-colors ease-in",
+        "[&_.icon-wrapper]:bg-transparent-white [&_.icon-wrapper]:rounded-full [&_.icon-wrapper]:px-2 [&_.icon-wrapper]:ml-2 [&_.icon-wrapper]:-mr-2",
+      ],
     },
     size: {
       small: "text-xs px-3 h-7",
@@ -25,9 +33,17 @@ const buttonClasses = cva("rounded-full inline-flex items-center", {
   },
 });
 
-export const Button = ({ children, href, variant, size }: ButtonProps) => {
+export const IconWrapper = ({ children }: { children: React.ReactNode }) => (
+  <span className="icon-wrapper">{children}</span>
+);
+
+export const Button = ({ children, href, variant, size, ...props }: ButtonProps) => {
   return (
-    <Link className={buttonClasses({ variant, size })} href={href}>
+    <Link
+      {...props}
+      className={buttonClasses({ variant, size, className: props.className })}
+      href={href}
+    >
       {children}
     </Link>
   );
