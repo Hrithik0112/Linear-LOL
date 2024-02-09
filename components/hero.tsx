d34998ac3d250da79637b59ad52854a212d15162
Hrithik <slumdog4321@gmail.com>
2024-02-09 13:00:36 +0530

    updated hero and footer

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,17 +1,26 @@
+import classNames from "classnames";
+
 interface HeroProps {
   children: React.ReactNode;
 }
 
 interface HeroElementProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-export const HeroTitle = ({ children }: HeroElementProps) => {
-  return <h1 className="text-5xl my-6">{children}</h1>;
+export const HeroTitle = ({ children, className }: HeroElementProps) => {
+  return (
+    <h1 className={classNames("text-gradient my-6 text-6xl md:text-8xl", className)}>{children}</h1>
+  );
 };
 
-export const HeroSubTitle = ({ children }: HeroElementProps) => {
-  return <p className="text-lg mb-12">{children}</p>;
+export const HeroSubtitle = ({ children, className }: HeroElementProps) => {
+  return (
+    <p className={classNames("mb-12 text-lg text-primary-text md:text-xl", className)}>
+      {children}
+    </p>
+  );
 };
 
 export const Hero = ({ children }: HeroProps) => {
